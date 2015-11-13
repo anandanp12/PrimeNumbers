6bf64cd728d138f837ae416527aff3304fb2e0cd
Anandan Palaniswamy <anandan.palaniswamy@aa.com>
2015-11-12 18:52:52 -0600

    Adding the code

---
+++ primenos.java
@@ -0,0 +1,21 @@
+class primenos
+{
+ public static void main(String args[])
+ {
+  int num,div,rem,flag;
+  for (num=3;num<100;num++)
+  {
+   flag=1;
+   for (div=2;div<=(num/2);div++)
+   {
+    rem=num%div;
+    if (rem==0)
+    flag=0;
+   }
+  if (flag==1)
+   System.out.println (num +" is a Prime Number");
+  else
+   System.out.println (num +" is not a Prime Number");
+  }
+ }
+}
